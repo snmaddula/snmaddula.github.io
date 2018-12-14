1065b6aaeee7880c5315414a80fa0acae1811a3d
snmaddula <sn_maddula@outlook.com>
2018-12-15 01:27:02 +0530

    Site updated: 2018-12-15 01:26:55

--- pf/AnsibleMagicLab_Post_Reboot.ps1
+++ pf/AnsibleMagicLab_Post_Reboot.ps1
@@ -2,11 +2,17 @@ echo "##########################################################################
 echo "######################## Ansible & Ansible Tower (AWX) Lab Setup #############################"
 echo "##########################       PART 02 (POST-REBOOT)      ##################################"
 echo "##############################################################################################"
-# Switch to Lab directory
-cd ~/__LAB__
+
+# Switch / Setup a Lab directory
+New-Item -ItemType Directory -Path ~/__LAB__ -Force; cd ~/__LAB__
+
 # Clone ansible magic repo
+Remove-Item -Path ansible-magic-lab -Recurse -ErrorAction Ignore
 cmd "/C git clone -b box https://github.com/snmaddula/ansible-magic-lab.git"; cd ansible-magic-lab
+
 # Spin up Lab
+cmd "/C vagrant destroy"
+cmd "/C vagrant box remove snmaddula/ubuntu"
 cmd "/c vagrant up"
 
 # Fetch AWX IP
@@ -15,6 +21,6 @@ vagrant ssh -c "hostname -I | cut -d ' ' -f2"
 
 echo "Congratulations! You're all set now!"
 echo "Use the above IP address in your browser to access Ansible Tower (AWX)"
-echo "CREDENTIALS:
+echo "CREDENTIALS:"
 echo "            username : admin"
 echo "            password : password"
